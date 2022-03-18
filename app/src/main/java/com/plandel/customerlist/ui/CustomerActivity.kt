5399852127ab82aa32f76db385b4653b3f9f40c0
kaiop <kaioplandel@gmail.com>
2022-03-18 00:37:19 -0300

    Create NewCustomerLayout Add dataBinding

--- app/src/main/java/com/plandel/customerlist/ui/CustomerActivity.kt
+++ app/src/main/java/com/plandel/customerlist/ui/CustomerActivity.kt
@@ -1,12 +1,28 @@
 package com.plandel.customerlist.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.plandel.customerlist.R
+import com.plandel.customerlist.databinding.ActivityCustomerBinding
+
 
 class CustomerActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCustomerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_customer)
+        binding = ActivityCustomerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupListeners()
+
+    }
+
+    private fun setupListeners() {
+        binding.buttonNewClient.setOnClickListener {
+            Intent(this, NewCustomerActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
